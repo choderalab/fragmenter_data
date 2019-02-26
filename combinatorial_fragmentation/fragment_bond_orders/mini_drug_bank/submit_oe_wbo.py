5a377291e7f8cae2be30dfb4f36bd1918216b7f8
ChayaSt <chaya.stern@choderalab.org>
2019-02-26 17:12:27 -0500

    fixed submit script

--- combinatorial_fragmentation/fragment_bond_orders/mini_drug_bank/submit_oe_wbo.py
+++ combinatorial_fragmentation/fragment_bond_orders/mini_drug_bank/submit_oe_wbo.py
@@ -6,20 +6,22 @@ import glob
 
 jobs_to_run = glob.glob('DrugBank*')
 
+name_seen = []
 for file in jobs_to_run:
-    print(file)
     name = file.split('_')[1].split('_')[0]
     name = 'DrugBank' + '_' +  name
-    print(name)
-    with open('oe_wbo_parent.lsf', 'r') as f:
-        filedata = f.read()
-    filedata = filedata.replace('JOB_NAME', name)
-    bsub_file = os.path.join(os.getcwd(), '{}_oe_wbo_parent.lsf'.format(name))
-    with open(bsub_file, 'w') as f:
-        f.write(filedata)
+    if name not in name_seen:
+        print(name)
+        name_seen.append(name)
+        with open('oe_wbo_parent.lsf', 'r') as f:
+            filedata = f.read()
+        filedata = filedata.replace('JOB_NAME', name)
+        bsub_file = os.path.join(os.getcwd(), '{}_oe_wbo_parent.lsf'.format(name))
+        with open(bsub_file, 'w') as f:
+            f.write(filedata)
 
-    #submit job
-    #stdin_file = open(bsub_file, 'r')
-    #subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
-    #stdin_file.close()
+        #submit job
+        #stdin_file = open(bsub_file, 'r')
+        #subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
+        #stdin_file.close()
 
