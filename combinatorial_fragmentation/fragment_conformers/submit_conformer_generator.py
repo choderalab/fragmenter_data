318954b13bb2dfae73e9ccab3553ff49b4fec76d
ChayaSt <chaya.stern@choderalab.org>
2019-06-20 15:38:16 -0400

    generate conformers for validation set fragments

--- combinatorial_fragmentation/fragment_conformers/submit_conformer_generator.py
+++ combinatorial_fragmentation/fragment_conformers/submit_conformer_generator.py
@@ -1,24 +1,24 @@
 import os
 import subprocess
 import glob
-jobs_to_run = ['Larotrectinib', 'Lenvatinib', 'Nilotinib', 'Palbociclib', 'Pazopanib', 'Ponatinib', 'Ribociclib',
-               'Sunitinib', 'Tofacitinib', 'Vemurafenib']
 
-jobs_to_run = glob.glob('../fragment/mini_drug_bank/*fragments.json')
+jobs_to_run = glob.glob('../fragment/validation_set/fragments_*.json')
 
 for file in jobs_to_run:
     name = file.split('/')[-1].split('.')[0].split('_')
-    name = name[0] + '_' + name[1]
+    name = name[1] + '_' + name[2]
     print(name)
     with open('conformer_generator.lsf', 'r') as f:
         filedata = f.read()
     filedata = filedata.replace('JOB_NAME', name)
-    bsub_file = os.path.join(os.getcwd(), '{}_conformer_generator.lsf'.format(name))
+    bsub_file = os.path.join(os.getcwd(), 'validation_set/{}_conformer_generator.lsf'.format(name))
     with open(bsub_file, 'w') as f:
         f.write(filedata)
 
     #submit job
     stdin_file = open(bsub_file, 'r')
+    os.chdir('validation_set/')
     subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
     stdin_file.close()
+    os.chdir('../')
 
