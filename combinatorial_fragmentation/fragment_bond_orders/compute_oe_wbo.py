a37ad0b647b926cc828ad463ac5230603c5cbff8
ChayaSt <chaya.stern@choderalab.org>
2019-06-21 19:50:03 -0400

    compute oe wbo for validation set fragments

--- combinatorial_fragmentation/fragment_bond_orders/compute_oe_wbo.py
+++ combinatorial_fragmentation/fragment_bond_orders/compute_oe_wbo.py
@@ -3,54 +3,43 @@ import json
 import cmiles
 from openeye import oechem, oequacpac
 import os
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.pyplot as plt
-import seaborn as sbn
+import warnings
 
 
-def compute_oe_wbo(molecule_input):
-    oe_wbo = {}
-    for frag in molecule_input:
-        print(frag)
-        oe_wbo[frag] = {}
-        # ensamble WBO
-        map_to_parent = molecule_input[frag]['provenance']['routine']['enumerate_fragments']['map_to_parent']
-        oemol = cmiles.utils.load_molecule(map_to_parent)
-        charged = fragmenter.chemi.get_charges(oemol)
-        for bond in charged.GetBonds():
-            bond_key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
-            oe_wbo[frag][bond_key] = {'ensamble': bond.GetData('WibergBondOrder')}
-            oe_wbo[frag][bond_key]['individual_conf'] = []
-        for i, json_mol in enumerate(molecule_input[frag]['initial_molecule']):
-            oemol = cmiles.utils.mol_from_json(json_mol)
-            # Add map to parent to keep track of bonds
-            #map_to_parent = ruxolitinib_inputs[frag]['provenance']['routine']['enumerate_fragments']['map_to_parent']
-            atom_map_to_parent = cmiles.utils.get_atom_map(oemol, map_to_parent, strict=False)
-            invert_map = cmiles.utils.invert_atom_map(atom_map_to_parent)
-            # Add parent map to frag
-            for atom in oemol.GetAtoms():
-                idx = atom.GetIdx()
-                if idx in invert_map:
-                    atom.SetMapIdx(invert_map[atom.GetIdx()])
-            #AM1 calculations
-            oemol_copy = oechem.OEMol(oemol)
-            #am1charges = oequacpac.OEAM1Charges(optimize=False)
-            if oequacpac.OEAssignPartialCharges(oemol_copy, oequacpac.OECharges_AM1BCCSym):
-                # Save WBO
-                for bond in oemol_copy.GetBonds():
-                    bond_key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
-                    try:
-                        oe_wbo[frag][bond_key]['individual_conf'].append(bond.GetData('WibergBondOrder'))
-                    except KeyError:
-                        if 0 in bond_key:
-                            oe_wbo[frag][bond_key] = {'individual_conf': [bond.GetData('WibergBondOrder')]}
-                        else:
-                            reverse_key = tuple(reversed(bond_key))
-                            oe_wbo[frag][reverse_key]['individual_conf'].append(bond.GetData('WibergBondOrder'))
-            else:
-                print('AM1BCC charging failed for {}, {}'.format(str(i), frag))
-    return oe_wbo
+def collect_wbos(molecule):
+    wbos = {}
+    for bond in molecule.GetBonds():
+        bond_key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
+        wbos[bond_key] = bond.GetData('WibergBondOrder')
+    return wbos
 
+def compute_wbos(map_to_parent):
+    oemol = cmiles.utils.load_molecule(map_to_parent)
+    try:
+        charged = fragmenter.chemi.get_charges(oemol, keep_confs=-1)
+    except:
+        print('Cannot charge {}'.format(oechem.OEMolToSmiles(charged)))
+        return False
+    # Collect all wbos
+    elf_wbo_estimate = collect_wbos(charged)
+    wbos = {key: {'elf_estimate': elf_wbo_estimate[key], 'individual_confs': []} for key in elf_wbo_estimate}
+    # Compute WBO for each conformer
+    for i, conf in enumerate(charged.GetConfs()):
+        mol_copy = oechem.OEMol(conf)
+        # Get WBO
+        if oequacpac.OEAssignPartialCharges(mol_copy, oequacpac.OECharges_AM1BCCSym):
+            ind_wbos = collect_wbos(molecule=mol_copy)
+            for b in ind_wbos:
+                if b not in wbos and 0 in b:
+                    wbos[b] = {'individual_confs': [ind_wbos[b]]}
+                elif b not in wbos:
+                    reverse = tuple(reversed(b))
+                    wbos[reverse]['individual_confs'].append(ind_wbos[b])
+
+                wbos[b]['individual_confs'].append(ind_wbos[b])
+        else:
+            print('AM1BCC charging failed for {}, {}'.format(str(i), i))
+    return charged, wbos
 
 def serialize(oe_wbo):
     serialized_wbo = {}
@@ -61,45 +50,47 @@ def serialize(oe_wbo):
             serialized_wbo[frag][key] = oe_wbo[frag][bond]
     return serialized_wbo
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description="calculate OE WBO")
-    parser.add_argument('-i', '--infile', type=str, help='Input JSON conformers file')
-    parser.add_argument('-n', '--name', type=str, help='Molecule name')
-    args = parser.parse_args()
-    infile = args.infile
-    name = args.name
+def organize_wbo(mapped_parent_smiles, oe_wbo, fragment_inputs):
 
-    with open('../filter/filtered_mini_drug_bank.json', 'r') as f:
-        kinase_inhibitors = json.load(f)
-    kinase_inhibitors[name] = kinase_inhibitors[name]
-
-    with open(infile, 'r') as f:
-        fragments_inputs = json.load(f)
-    oe_wbo = compute_oe_wbo(fragments_inputs)
-    serialized = serialize(oe_wbo)
-    try:
-        os.mkdir(name)
-    except FileExistsError:
-        print('{} directory already exists. Files will be overwritten'.format(name))
-    with open('{}/{}_oe_wbo.json'.format(name, name), 'w') as f:
-        json.dump(serialized, f, indent=2, sort_keys=True)
-
-    # organize for easier plotting
-    # organize so it's easier to make plots
-    mapped_parent_smiles = kinase_inhibitors[name]['canonical_isomeric_explicit_hydrogen_mapped_smiles']
     mapped_parent_mol = cmiles.utils.load_molecule(mapped_parent_smiles)
-    fragmenter.chemi.mol_to_image_atoms_label(mapped_parent_smiles, '{}/mapped_{}.png'.format(name, name))
+    # Find all relevant bonds
     rot_bonds = []
     for bond in mapped_parent_mol.GetBonds():
-        if bond.IsRotor():
-            key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
-            rot_bonds.append(key)
+        if bond.IsInRing():
+            continue
+        # keep bonds that do not include H
+        a1 = bond.GetBgn()
+        a2 = bond.GetEnd()
+        if a1.IsHydrogen() or a2.IsHydrogen():
+            continue
+        key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
+        rot_bonds.append(key)
     frag_with_bond = {b:{} for b in rot_bonds }
+
     for frag in oe_wbo:
-        map_to_parent = fragments_inputs[frag]['provenance']['routine']['enumerate_fragments']['map_to_parent']
-        oemol = cmiles.utils.load_molecule(map_to_parent)
-        r_bonds = [(b.GetBgn().GetMapIdx(), b.GetEnd().GetMapIdx()) for b in oemol.GetBonds() if b.IsRotor()]
+        frag_name = frag.split('_')
+        if len(frag_name) > 1:
+            index = frag_name[-1]
+        else:
+            index = 0
+        frag_name = frag_name[0]
+        if not index == 'parent':
+            map_to_parent = fragment_inputs[frag_name]['provenance']['routine']['enumerate_fragments']['map_to_parent'][int(index)]
+            oemol = cmiles.utils.load_molecule(map_to_parent)
+        elif index == 'parent':
+            oemol = cmiles.utils.load_molecule(mapped_parent_smiles)
+        #r_bonds = [(b.GetBgn().GetMapIdx(), b.GetEnd().GetMapIdx()) for b in oemol.GetBonds() if b.IsRotor()]
+        r_bonds = []
+        for bond in oemol.GetBonds():
+            if bond.IsInRing():
+                continue
+            # keep bonds that do not include H
+            a1 = bond.GetBgn()
+            a2 = bond.GetEnd()
+            if a1.IsHydrogen() or a2.IsHydrogen():
+                continue
+            key = (bond.GetBgn().GetMapIdx(), bond.GetEnd().GetMapIdx())
+            r_bonds.append(key)
         for b in r_bonds:
             reverse = tuple(reversed(b))
             if b not in oe_wbo[frag]:
@@ -109,107 +100,90 @@ if __name__ == '__main__':
                 bo = oe_wbo[frag][b]
             try:
                 frag_with_bond[b][frag] = bo
+                frag_with_bond[b][frag]['map_to_parent'] = map_to_parent
             except KeyError:
                 frag_with_bond[reverse][frag] = bo
+                frag_with_bond[reverse][frag]['map_to_parent'] = map_to_parent
     serialized = {}
     for bond in frag_with_bond:
         key = fragmenter.workflow_api.serialize_key(bond)
         serialized[key] = frag_with_bond[bond]
-    with open('{}/{}_oe_wbo_by_bond.json'.format(name, name), 'w') as f:
-        json.dump(serialized, f, indent=2, sort_keys=True)
-
-    # sort fragments by wbo
-    sorted_frags = {}
-    for b in frag_with_bond:
-        list_1 = []
-        list_2 = []
-        for frag in frag_with_bond[b]:
-            list_1.append(frag)
-            list_2.append(frag_with_bond[b][frag]['ensamble'])
-            sorted_frags[b] = [x for _,x in sorted(zip(list_2, list_1))]
-
-    rot_bonds = list(frag_with_bond.keys())
-
-    # plot results on one pdf page
-    with PdfPages('{}/{}_fragment_bond_orders.pdf'.format(name, name)) as pdf:
-        for b in rot_bonds:
-            #b = rot_bonds[3]
-            n = len(frag_with_bond[b])
-
-            fig, axes = plt.subplots(n, 1)
-            fig.dpi = 400
-            x_min = 3
-            x_max = 0
-            for f in frag_with_bond[b]:
-                wbo = frag_with_bond[b][f]['individual_conf']
-                if min(wbo) < x_min:
-                    x_min = min(wbo)
-                if max(wbo) > x_max:
-                    x_max = max(wbo)
-
-            for i, frag in enumerate(sorted_frags[b]):
-                wbo = frag_with_bond[b][frag]['individual_conf']
-
-                wbo_s = frag_with_bond[b][frag]['ensamble']
-                ax = plt.subplot(n, 1, i+1)
-                ax.spines['left'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-                ax.spines['top'].set_visible(False)
-                ax.spines['bottom'].set_visible(False)
-                ax.patch.set_facecolor('none')
-                sbn.kdeplot(wbo, shade= True, alpha=0.8)
-                sbn.distplot(wbo, hist=False, rug=True, kde=False, color='black')
-                sbn.kdeplot(wbo, lw=1, color='black')
-                plt.axvline(x=wbo_s, ymin=0, ymax=1, color='black', linewidth=0.5)
-                #if i == 0:
-                    #sbn.kdeplot(wbo)
-                    #sbn.kdeplot(bond_order_std_aromatic_unopt['Imatinib']['wiberg_bo'][bond], shade=True, label='non-optimized')
-                    #try:
-                    #    key = (bond[0]-1, bond[1]-1)
-                    #    sbn.kdeplot(oe_wbo[key], shade=True, label='openeye')
-                    #except KeyError:
-                    #    key = (bond[-1]-1, bond[0]-1)
-                    #    sbn.kdeplot(oe_wbo[key], shade=True, label='openeye')
-                #else:
-                #    sbn.kdeplot(bond_order_std_aromatic_opt['Imatinib']['wiberg_bo'][bond], shade=True)
-                #    sbn.kdeplot(bond_order_std_aromatic_unopt['Imatinib']['wiberg_bo'][bond], shade=True)
-                #    try:
-                 #       key = (bond[0]-1, bond[1]-1)
-                 #       sbn.kdeplot(oe_wbo[key], shade=True)
-                  #  except KeyError:
-                  #      key = (bond[-1]-1, bond[0]-1)
-                  #      sbn.kdeplot(oe_wbo[key], shade=True)
-                plt.xlim(x_min-0.05, x_max+0.05)
-                plt.yticks([])
-                #ax.set_yticklabels(bond_order_std_rot_bonds_opt['Imatinib']['bonds'], fontsize=6, rotation=0)
-                #ax.yaxis.grid(False)
-                ax.yaxis.set_label_coords(-0.05, 0)
-                plt.ylabel(i, rotation=0, size=8)
-                if i != n-1:
-                    plt.xticks([])
-                else:
-                    plt.xlabel('Bond order')
-                if i == 0:
-                    #plt.legend(prop={'size': 10}, bbox_to_anchor=(1.35, 1))
-                    plt.title('bond {}'.format(b))
-            overlap=0.5
-            h_pad = 5 + (- 5*(1 + overlap))
-            fig.tight_layout(h_pad=h_pad)
-            pdf.savefig(bbox_inches='tight')
-            plt.close()
-
-    for b in sorted_frags:
-        for i, frag in enumerate(sorted_frags[b]):
-            try:
-                bo = oe_wbo[frag][b]['ensamble']
-            except KeyError:
-                bo = oe_wbo[frag][(b[1], b[0])]['ensamble']
-            map_from_parent = fragments_inputs[frag]['provenance']['routine']['enumerate_fragments']['map_to_parent']
-            #b_1 = b.split('[')[-1].split(']')[0].split(',')
-            #b_2 = (int(b_1[0]), int(b_1[1]))
-            fragmenter.chemi.highlight_bond_by_map_idx(map_from_parent, [b], wbo=bo,
-                                                       fname='{}/frag_{}_bond_{}_{}.png'.format(name, i, b[0], b[1]))
+    return serialized
 
 
 
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="calculate OE WBO")
+    parser.add_argument('-i', '--infile', type=str, help='Input JSON conformers file')
+    parser.add_argument('-n', '--name', type=str, help='Molecule name')
+    args = parser.parse_args()
+    infile = args.infile
+    name = args.name
 
+    # Get parent
+    index = name.split('_')[-1]
+    with open('../../enumerate_states/states_{}.json'.format(name.split('_')[0]), 'r') as f:
+        states = json.load(f)['states']
+    mapped_parent_smiles = states[int(index)]
+    mapped_parent_mol = oechem.OEMol()
+    oechem.OESmilesToMol(mapped_parent_mol, mapped_parent_smiles)
+    cmiles_identifiers = cmiles.get_molecule_ids(mapped_parent_smiles)
+    # Check that both mapped SMILES are the same
+    if not mapped_parent_smiles == cmiles_identifiers['canonical_isomeric_explicit_hydrogen_mapped_smiles']:
+        warnings.warn('mapped smiles do not match. Replacing with original mapped SMILES')
+        cmiles_identifiers['canonical_isomeric_explicit_hydrogen_mapped_smiles'] = mapped_parent_smiles
+
+    # compute parent WBO (and generate conformers)
+    charged, parent_wbos = compute_wbos(mapped_parent_smiles)
+    qcschema_molecules = [cmiles.utils.mol_to_map_ordered_qcschema(conf, mapped_parent_smiles) for conf in charged.GetConfs()]
+    # Save parent conformers
+    qcarchive_input = {'initial_molecules': qcschema_molecules,
+                       'cmiles_identifiers': cmiles_identifiers}
+    fname = '../../fragment_conformers/validation_set/{}_parent_conformers.json'.format(name)
+
+    with open(fname, 'w') as f:
+        json.dump(qcarchive_input, f, indent=2, sort_keys=True)
+
+    # Get wbo and conformers for fragments
+    with open(infile, 'r') as f:
+        fragments = json.load(f)
+    all_wbos = {}
+    all_conformers = {}
+    for frag in fragments:
+        mol_id = fragments[frag]['identifiers']
+        provenance = fragments[frag]['provenance']
+        map_to_parents = fragments[frag]['provenance']['routine']['enumerate_fragments']['map_to_parent']
+        for i, map_to_parent in enumerate(map_to_parents):
+            if i > 0:
+                frag_name = '{}_{}'.format(frag, str(i))
+            else:
+                frag_name = frag
+
+            charged, all_wbos[frag] = compute_wbos(map_to_parent)
+            all_wbos[frag]['map_to_parent'] = map_to_parent
+            mapped_smiles = mol_id['canonical_isomeric_explicit_hydrogen_mapped_smiles']
+            qcschema_molecules = [cmiles.utils.mol_to_map_ordered_qcschema(conf, mapped_smiles) for conf in charged.GetConfs()]
+            all_conformers[frag_name] = {'initial_molecules': qcschema_molecules,
+                                         'cmiles_identifiers': mol_id,
+                                         'provenance': provenance}
+    # Save conformers
+    fname = '../../fragment_conformers/validation_set/{}_conformers.json'.format(name)
+    with open(fname, 'w') as f:
+        json.dump(all_conformers, f, sort_keys=True, indent=2)
+
+    # Save all oe wbo
+    all_wbos['{}_parent'.format(cmiles_identifiers['canonical_isomeric_smiles'])] = parent_wbos
+    serialized_wbo = serialize(all_wbos)
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+         print('{} directory already exists. Files will be overwritten'.format(name))
+    with open('{}/{}_oe_wbo.json'.format(name, name), 'w') as f:
+        json.dump(serialized_wbo, f, indent=2, sort_keys=True)
+
+    # Organize wbos for easier analysis
+    organized_wbo = organize_wbo(mapped_parent_smiles, all_wbos, fragments)
+    fname = '{}/{}_oe_wbo_by_bond.json'.format(name, name)
+    with open(fname, 'w') as f:
+        json.dump(organized_wbo, f, indent=2, sort_keys=True)
--- combinatorial_fragmentation/fragment_bond_orders/submit_oe_wbo.py
+++ combinatorial_fragmentation/fragment_bond_orders/submit_oe_wbo.py
@@ -4,21 +4,23 @@ import glob
 # jobs_to_run = ['Larotrectinib', 'Lenvatinib', 'Nilotinib', 'Palbociclib', 'Pazopanib', 'Ponatinib', 'Ribociclib',
 #                'Sunitinib', 'Tofacitinib', 'Vemurafenib']
 
-jobs_to_run = glob.glob('../fragment_conformers/mini_drug_bank/*_bo_input.json')
+jobs_to_run = glob.glob('../fragment/validation_set/fragments*')
 
 for file in jobs_to_run:
     name = file.split('/')[-1].split('.')[0].split('_')
-    name = name[0]
+    name = name[1] + '_' +  name[2]
     print(name)
     with open('oe_wbo.lsf', 'r') as f:
         filedata = f.read()
     filedata = filedata.replace('JOB_NAME', name)
-    bsub_file = os.path.join(os.getcwd(), 'mini_drug_bank/{}_oe_wbo.lsf'.format(name))
+    bsub_file = os.path.join(os.getcwd(), 'validation_set/{}_oe_wbo.lsf'.format(name))
     with open(bsub_file, 'w') as f:
         f.write(filedata)
 
     #submit job
-    #stdin_file = open(bsub_file, 'r')
-    #subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
-    #stdin_file.close()
+    os.chdir(os.path.join(os.getcwd(), 'validation_set'))
+    stdin_file = open(bsub_file, 'r')
+    subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
+    stdin_file.close()
+    os.chdir('..')
 
