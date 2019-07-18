a9bb7c23913339aba78d4ee6af30654a465afd3f
ChayaSt <chaya.stern@choderalab.org>
2019-07-18 19:42:54 -0400

    enumerate reasonable tautomer states

--- combinatorial_fragmentation/enumerate_states/enumerate_states.py
+++ combinatorial_fragmentation/enumerate_states/enumerate_states.py
@@ -1,74 +1,51 @@
 import fragmenter
+from openeye import oechem
 import pandas as pd
-from qcfractal import interface as ptl
 import json
+import socket
+import getpass
 
-validation_set = pd.read_csv('../filter/validation_set.csv')
-client = ptl.FractalClient('https://localhost:7777/', verify=False)
+validation_set_input = pd.read_csv('../filter/validation_set/drugbank_filtered.csv')
 
-workflow = {
-    "validation_set": {
-        "fragmenter":{
-          "enumerate_states": {
-            "version": fragmenter.__version__,
-            "options": {
-              "protonation": True,
-              "tautomers": False,
-              "stereoisomers": False,
-              "max_states": 10,
-              "level": 0,
-              "reasonable": True,
-              "carbon_hybridization": True,
-              "suppress_hydrogen": True,
-              "mapped": True
-            }
-          },
-          "enumerate_fragments": {
-            "version": fragmenter.__version__,
-            "scheme": "combinatorial",
-            "functional_groups": False,
-            "options":{},
-          },
-        "torsiondrive_input": {},
-        "torsiondrive_static_options": {
-        "optimization_spec": {
-          "program": "geometric",
-          "keywords": {
-            "coordsys": "tric"
-          }
-        },
-        "qc_spec": {
-          "driver": "gradient",
-          "method": "UFF",
-          "basis": "",
-          "keywords": None,
-          "program": "rdkit"
-        },
-        "keywords": {}
-      },
-      "optimization_static_options":{
-          "program": "geometric",
-          "keywords": {
-            "coordsys": "tric"
-          },
-        "qc_spec":{
-           "driver": "gradient",
-          "method": "UFF",
-          "basis": "",
-          "keywords": None,
-          "program": "rdkit"
-        }
-      }
-    }
-  }
-}
+# Clean up names = remove space because it causes bash scripts to crash
+for name in validation_set_input['name']:
+    if ' ' in name:
+        name_split = name.split(' ')
+        new_name = ''
+        for n in name_split[:-1]:
+            new_name += n 
+            new_name += '_'
+        new_name += name_split[-1]
+        validation_set_input.replace(to_replace=name, value=new_name, inplace=True)
+    if name[0].isdigit():
+        new_name = 'four' + name[1:]
+        name = new_name
+        validation_set_input.replace(to_replace=name, value=new_name, inplace=True)
 
+provenance = {}
+provenance['creator'] = "fragmenter"
+provenance['hostname'] = socket.gethostname()
+provenance['username'] = getpass.getuser()
+provenance['routine'] = {'enumerate_states' : {'version': fragmenter.__version__,
+                        'options': {'stereoisomers': False, 'explicit_h': False, 'filter_nitro': True}}}
 
-with open('../validation_set_workflow.json', 'w') as f:
-    json.dump(workflow, f, sort_keys=True, indent=2)
+all_states =[]
+all_names = []
+for smiles, name in zip(validation_set_input.smiles, validation_set_input.name):
+    mol = oechem.OEMol()
+    oechem.OESmilesToMol(mol, smiles)
+    mol.SetTitle(name)
+    
+    states, names = fragmenter.fragment.enumerate_states(mol,stereoisomers=False, explicit_h=False, return_names=True,
+                                                   filter_nitro=True)
 
-fragmenter_workflow = fragmenter.workflow_api.WorkFlow(workflow_id='validation_set', client=client,
-                                                       workflow_json='../validation_set_workflow.json')
+    states_dict = {'provenance': provenance, 'states': states}
+    states_dict['provenance']['routine']['enumerate_states']['parent_molecule_name'] = name
+    states_dict['provenance']['routine']['enumerate_states']['parent_molecule'] = smiles
+    with open('validation_set/{}_states.json'.format(name), 'w') as f:
+        json.dump(states_dict, f, indent=2, sort_keys=True)
+    all_states.extend(states)
+    all_names.extend(names)
 
-for smiles, name in zip(validation_set.smiles, validation_set.name):
-    fragmenter_workflow.enumerate_states(smiles, title=name, json_filename='states_{}.json'.format(name))
+fragmenter.chemi.to_pdf(molecules=all_states, names=all_names, fname='enumerated_states.pdf')
+print('Number of molelcules to fragment: {}'.format(len(all_states)))
