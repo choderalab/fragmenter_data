378463fc2765b5b9784768462a13de7aee8a88c3
ChayaSt <chaya.stern@choderalab.org>
2019-07-26 10:18:45 -0400

    select molecules for torsiondrives and generate input

--- phenyl_benchmark/generate_torsiondrive_inputs.py
+++ phenyl_benchmark/generate_torsiondrive_inputs.py
@@ -1,8 +1,11 @@
-from openeye import oechem
 import cmiles
+import openeye
+import fragmenter
+
 import json
-from fragmenter import torsions, chemi
+
 import matplotlib.colors as mcolors
+
 
 
 def add_atom_map(mol):
@@ -19,7 +22,7 @@ def add_atom_map(mol):
 
     """
     # copy molecule
-    mol = oechem.OEMol(mol)
+    mol = openeye.oechem.OEMol(mol)
     if not cmiles.utils.has_explicit_hydrogen(mol):
         cmiles.utils.add_explicit_hydrogen(mol)
     cmiles._cmiles_oe.canonical_order_atoms(mol, in_place=True)
@@ -88,7 +91,7 @@ color_keys = ['rosybrown', 'indianred', 'red', 'orange', 'gold', 'yellow','green
           'palevioletred', 'lightpink']
 colors = mcolors.CSS4_COLORS
 
-n = 0
+td_inputs = {}
 for i, fgroup in enumerate(fgroups):
     with open('data/{}_R1_wbos.json'.format(fgroup), 'r') as f:
         wbos = json.load(f)
@@ -108,15 +111,16 @@ for i, fgroup in enumerate(fgroups):
     if len(wbos_filtered) < 2:
         print('WBO range is too small. Dropping {}'.format(fgroup))
         continue
-    if len(wbos_filtered) > 12:
+    if len(wbos_filtered) > 10:
         # remove some
-        to_remove = wbos_filtered[2::int(len(wbos_filtered)/(len(wbos_filtered) - 12))]
+        to_remove = wbos_filtered[1::int(len(wbos_filtered)/(len(wbos_filtered) - 10))]
         wbo_filtered_2 = []
         for j in wbos_filtered:
             if j in to_remove:
                 continue
             else:
                 wbo_filtered_2.append(j)
+        wbo_filtered_2.append(wbos_filtered[-1])
 
         #wbo_filtered = list(filter(lambda a: a in to_remove, wbos_filtered))
         wbos_filtered = wbo_filtered_2
@@ -124,32 +128,51 @@ for i, fgroup in enumerate(fgroups):
     smiles = [wbo[-1] for wbo in wbos_filtered]
 
     wbos = [wbo[0] for wbo in wbos_filtered]
-    c_oe = oechem.OEColor(colors[color_keys[i]])
-    chemi.to_pdf(smiles, fname='figures/qcarchive_torsiondrives/{}_to_drive.pdf'.format(fgroup), bo=wbos, bond_map_idx=(4, 5),
+    c_oe = openeye.oechem.OEColor(colors[color_keys[i]])
+    fragmenter.chemi.to_pdf(smiles, fname='figures/qcarchive_torsiondrives/{}_to_drive.pdf'.format(fgroup), bo=wbos, bond_map_idx=(4, 5),
                  align=smiles[0], color=c_oe)
 
 
     print(fgroup, len(wbos_filtered))
-    n += len(wbos_filtered)
-    td_input = {}
     job_indices = []
     for wbo, name, smiles in wbos_filtered:
-        mol = oechem.OEMol()
-        oechem.OESmilesToMol(mol, smiles)
+        mol =openeye.oechem.OEMol()
+        openeye.oechem.OESmilesToMol(mol, smiles)
         mapped_mol = add_atom_map(mol)
 
         # Find bond
         central_bond = find_central_bond(mapped_mol)
-        torsion = torsions.find_torsion_around_bond(mapped_mol, bond=central_bond)
+        torsion = fragmenter.torsions.find_torsion_around_bond(mapped_mol, bond=central_bond)
         # get job index. Save job indices and WBOs for each fgroup - this will make analysis easier
-        mapped_smiles = oechem.OEMolToSmiles(mapped_mol)
-        job_index = cmiles.utils.to_canonical_label(oechem.OEMolToSmiles(mapped_mol), torsion)
+        mapped_smiles = openeye.oechem.OEMolToSmiles(mapped_mol)
+        job_index = cmiles.utils.to_canonical_label(openeye.oechem.OEMolToSmiles(mapped_mol), torsion)
         job_indices.append([job_index, torsion, name, wbo])
+
+        # Generate conformers
+        conformers = fragmenter.chemi.generate_conformers(mapped_mol, dense=True)
+        cmiles_identifiers = cmiles.get_molecule_ids(mapped_smiles)
+
+        # Check that mapping did not change
+        if openeye.oechem.OEMolToSmiles(conformers) != mapped_smiles or mapped_smiles != cmiles_identifiers['canonical_isomeric_explicit_hydrogen_mapped_smiles']:
+            print('Map changed for {}'.format(job_index))
+        qcarchive_mols = [cmiles.utils.mol_to_map_ordered_qcschema(conformer, mapped_smiles) for conformer in conformers.GetConfs()]
+        td_inputs[job_index] = {'input_molecules': qcarchive_mols,
+                               'grid': [15],
+                               'dihedral': torsion,
+                               'cmiles_identifiers':  cmiles.get_molecule_ids(mapped_smiles),
+                               'provenance': {'fragmenter_version': fragmenter.__version__,
+                                              'openeye_version:': openeye.__version__,
+                                              'dataset': {'fgroup': fgroup,
+                                                          'name': name,
+                                                          'oe_wbo': wbo}}
+                               }
 
     with open('data/{}_td_job_indices.json'.format(fgroup), 'w') as f:
         json.dump(job_indices, f, indent=2, sort_keys=True)
 
-        # Prepare inputs in one large JSON
+with open('data/phenyl_set_torsiondrive_inputs.json', 'w') as f:
+    json.dump(td_inputs, f, indent=2, sort_keys=True)
 
-print(n)
+print('Total number of jobs')
+print(len(td_inputs))
 
