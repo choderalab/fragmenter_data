e8369ed299f0c8e33ed28da66a0f61d779e69cd3
ChayaSt <chaya.stern@choderalab.org>
2019-07-27 21:40:01 -0400

    updated torsiondrive inputs to exclude conjested td scans and get better distribution over range of WBO

--- phenyl_benchmark/generate_torsiondrive_inputs.py
+++ phenyl_benchmark/generate_torsiondrive_inputs.py
@@ -7,7 +7,6 @@ import json
 import matplotlib.colors as mcolors
 
 
-
 def add_atom_map(mol):
     """
     Add atom map to molecule while saving the 4-5 atom map (which specifies the central bond we are interested in
@@ -53,6 +52,7 @@ def find_central_bond(mol):
                 central_bond.append(atom.GetMapIdx())
     return tuple(central_bond)
 
+# Do not drive trimethylamonium and trifluoromethyl because it is too congested
 fgroups =  [
     'dimethylamino',
     'methylamino',
@@ -71,13 +71,11 @@ fgroups =  [
     'carbamate',
     'benzoicacid',
     'ethoxycarbonyl',
-    'trifluoromethyl',
-    'trimethylamonium',
     'nitro']
 
 color_keys = ['rosybrown', 'indianred', 'red', 'orange', 'gold', 'yellow','greenyellow', 'green', 'limegreen',
-          'lightseagreen', 'teal', 'cyan', 'deepskyblue', 'royalblue', 'mediumslateblue', 'blueviolet', 'mediumorchid', 'violet',
-          'palevioletred', 'lightpink']
+          'lightseagreen', 'teal', 'cyan', 'deepskyblue', 'royalblue', 'mediumslateblue', 'blueviolet', 'mediumorchid',
+           'lightpink']
 colors = mcolors.CSS4_COLORS
 
 # The indices here are slightly different than the indices already on QCArchive. This is to make sure we do not rerun them.
@@ -94,36 +92,46 @@ to_replace = {'amino': ('[H:4][NH:3][c:2]1[cH:1]cc(nc1)[N+](C)(C)C', '[H:4][NH:3
             'methylamino':
             ('[CH3:4][NH:3][c:2]1ccc(n[cH:1]1)[N+](C)(C)C', '[CH3:4][NH:3][c:2]1[cH:1]cc(nc1)[N+](C)(C)C'),
             'phenylurea':
-            ('C[N+](C)(C)c1c[cH:1][c:2](cn1)[NH:3][C:4](=O)N', 'C[N+](C)(C)c1c[cH:1][c:2](cn1)[NH:3][C:4](=O)N'),
+            [('C[N+](C)(C)c1cc[c:2]([cH:1]n1)[NH:3][C:4](=O)N', 'C[N+](C)(C)c1c[cH:1][c:2](cn1)[NH:3][C:4](=O)N'),
+            ('c1cc([cH:1][c:2](c1)[NH:3][C:4](=O)N)C(=O)O', 'c1cc(c[c:2]([cH:1]1)[NH:3][C:4](=O)N)C(=O)O')],
             'urea':
-            ('CN[C:4](=O)[NH:3][c:2]1ccc(n[cH:1]1)[N+](C)(C)C', 'CN[C:4](=O)[NH:3][c:2]1[cH:1]cc(nc1)[N+](C)(C)C')}
-
+            ('CN[C:4](=O)[NH:3][c:2]1ccc(n[cH:1]1)[N+](C)(C)C', 'CN[C:4](=O)[NH:3][c:2]1[cH:1]cc(nc1)[N+](C)(C)C'),
+            'ethoxycarbonyl':
+            ('CC[O:4][C:3](=O)[c:2]1ccnc([cH:1]1)[N+](C)(C)C', 'CCO[C:3](=[O:4])[c:2]1[cH:1]cnc(c1)[N+](C)(C)C'),
+            'ethylamide':
+            ( '[H:4][N:3]([c:2]1ccc(c[cH:1]1)F)C(=O)CC','CC[C:4](=O)[NH:3][c:2]1[cH:1]cc(cc1)F'),
+             'nitro':
+             ('C[N+](C)(C)c1c[c:2]([cH:1]cn1)[N+:3](=[O:4])[O-]', 'C[N+](C)(C)c1c[c:2]([cH:1]cn1)[N+:3](=O)[O-:4]'),
+             'benzoicacid':
+             ('C[N+](C)(C)c1[cH:1][c:2](ccn1)[C:3](=O)[OH:4]', 'C[N+](C)(C)c1c[c:2]([cH:1]cn1)[C:3](=[O:4])O')
+            }
 td_inputs = {}
 for i, fgroup in enumerate(fgroups):
     print(fgroup)
     with open('data/{}_R1_wbos.json'.format(fgroup), 'r') as f:
         wbos = json.load(f)
 
-    # Select molecules (up to 15) that are evenly distributed along WBOs
-    wbos_dist = [round(wbo[0], 2) for wbo in wbos]
-    differences = [j-i for i, j in zip(wbos_dist[:-1], wbos_dist[1:])]
-    indices = [i for i, j in enumerate(differences) if j > 0]
-    selected_mols = [wbos[i] for i in indices]
-    selected_mols.append(wbos[-1])
-
-    # Remove phenoxide because the negative charge is hard to deal with
     wbos_filtered = []
-    for wbo, name, smiles in selected_mols:
+    for wbo, name, smiles in wbos:
         if not 'phenoxide' in name:
             wbos_filtered.append([wbo, name, smiles])
-    if len(wbos_filtered) < 2:
+    # Select molecules (up to 15) that are evenly distributed along WBOs
+    wbos_dist = [round(wbo[0], 2) for wbo in wbos_filtered]
+    differences = [j-i for i, j in zip(wbos_dist[:-1], wbos_dist[1:])]
+    indices = [i for i, j in enumerate(differences) if j > 0]
+    selected_mols = [wbos_filtered[i+1] for i in indices]
+    selected_mols.append(wbos_filtered[-1])
+    selected_mols.insert(0, wbos_filtered[0])
+
+
+    if len(selected_mols) < 3:
         print('WBO range is too small. Dropping {}'.format(fgroup))
         continue
-    if len(wbos_filtered) > 10:
+    if len(selected_mols) > 10:
         # remove some
-        to_remove = wbos_filtered[1::int(len(wbos_filtered)/(len(wbos_filtered) - 10))]
+        to_remove = selected_mols[1::int(round(len(selected_mols)/(len(selected_mols) - 10)))]
         wbo_filtered_2 = []
-        for j in wbos_filtered:
+        for j in selected_mols:
             if j in to_remove:
                 continue
             else:
@@ -131,19 +139,23 @@ for i, fgroup in enumerate(fgroups):
         wbo_filtered_2.append(wbos_filtered[-1])
 
         #wbo_filtered = list(filter(lambda a: a in to_remove, wbos_filtered))
-        wbos_filtered = wbo_filtered_2
+        selected_mols = wbo_filtered_2
+    if selected_mols[-1][0] - selected_mols[-2][0] < 0.01:
+        selected_mols.remove(selected_mols[-2])
+    if selected_mols[1][0] - selected_mols[0][0] < 0.01:
+        selected_mols.remove(selected_mols[1])
 
-    smiles = [wbo[-1] for wbo in wbos_filtered]
+    smiles = [wbo[-1] for wbo in selected_mols]
 
-    wbos = [wbo[0] for wbo in wbos_filtered]
+    wbos = [wbo[0] for wbo in selected_mols]
     c_oe = openeye.oechem.OEColor(colors[color_keys[i]])
     fragmenter.chemi.to_pdf(smiles, fname='figures/qcarchive_torsiondrives/{}_to_drive.pdf'.format(fgroup), bo=wbos, bond_map_idx=(4, 5),
                  align=smiles[0], color=c_oe)
 
 
-    print(fgroup, len(wbos_filtered))
+    print(fgroup, len(selected_mols))
     job_indices = []
-    for wbo, name, smiles in wbos_filtered:
+    for wbo, name, smiles in selected_mols:
         mol =openeye.oechem.OEMol()
         openeye.oechem.OESmilesToMol(mol, smiles)
         mapped_mol = add_atom_map(mol)
@@ -155,9 +167,11 @@ for i, fgroup in enumerate(fgroups):
         mapped_smiles = openeye.oechem.OEMolToSmiles(mapped_mol)
         job_index = cmiles.utils.to_canonical_label(openeye.oechem.OEMolToSmiles(mapped_mol), torsion)
         if fgroup in to_replace:
-
+            if isinstance(to_replace[fgroup], list):
+                for index in to_replace[fgroup]:
+                    if job_index == index[0]:
+                        job_index = index[1]
             if job_index == to_replace[fgroup][0]:
-                print('replacing index for {}'.format(name))
                 job_index = to_replace[fgroup][1]
         job_indices.append([job_index, torsion, name, wbo])
 
