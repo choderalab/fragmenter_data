a9829facccf977d590694df081a8c23d25cbf497
ChayaSt <chaya.stern@choderalab.org>
2019-07-29 18:46:17 -0400

    save all sensetive bonds

--- combinatorial_fragmentation/rank_fragments/select_validation_set.py
+++ combinatorial_fragmentation/rank_fragments/select_validation_set.py
@@ -1,6 +1,6 @@
 import json
 import glob
-from fragmenter import chemi
+from fragmenter import chemi, utils
 from openeye import oechem
 import os
 import shutil
@@ -10,6 +10,12 @@ import shutil
 mols = glob.glob('validation_set/*/')
 max_scores = []
 failures = []
+
+try:
+    os.mkdir('selected')
+except FileExistsError:
+    print("overwriting selected")
+
 for name in mols:
     name = name.split('/')[1]
     print(name)
@@ -21,11 +27,15 @@ for name in mols:
         failures.append(name)
         continue
     max_score = scores['greatest_discrepancy']
+    selected_bonds = []
     for b in scores:
         if b == 'greatest_discrepancy':
             continue
         if max_score in scores[b]:
-            bond = [int(i) for i in b.split('[')[-1].split(']')[0].split(',')]
+            max_bond = [int(i) for i in b.split('[')[-1].split(']')[0].split(',')]
+        if max(scores[b]) > 0.001:
+            selected_bond = utils.deserialize_bond(b)
+            selected_bonds.append(selected_bond)
     with open('validation_set/{}/{}_oe_wbo_with_score.json'.format(name, name), 'r') as f:
         frags = json.load(f)
         for bo in frags:
@@ -41,21 +51,24 @@ for name in mols:
                     break
             break
 
-    max_scores.append((max_score, name, charge, parent_mol, bond))
+    max_scores.append((max_score, name, charge, parent_mol, selected_bonds, parent_smiles))
 print(len(max_scores))
 
 # sort mol and visualize top 50 molecules.
 sorted_scores = sorted(max_scores, reverse=True)
 sorted_mols = [i[3] for i in sorted_scores[:100]]
-sorted_bonds = [i[-1] for i in sorted_scores[:100]]
+sorted_bonds = [i[4] for i in sorted_scores[:100]]
+sorted_smiles = [i[5] for i in sorted_scores[:100]]
 chemi.to_pdf(sorted_mols, fname='selected_validation_set.pdf',  bond_map_idx=sorted_bonds)
 
-try:
-    os.mkdir('selected')
-except FileExistsError:
-    print("overwriting selected")
 validation_set = {}
 for mol in sorted_scores[:100]:
     shutil.copytree('validation_set/{}'.format(mol[1]), 'selected/{}'.format(mol[1]))
-
+for i, mol in enumerate(sorted_mols):
+    selected_bonds = {}
+    selected_bonds['bonds'] = sorted_bonds[i]
+    selected_bonds['parent_smiles'] = sorted_smiles
+    name = mol.GetTitle()
+    with open('selected/{}/{}_selected_bonds.json', 'w') as f:
+        json.dump(selected_bonds, f, sort_keys=True, indent=2)
 
