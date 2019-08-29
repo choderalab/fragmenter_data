1b69692651e27e42d442ebad04c99ed51f28d5bc
ChayaSt <chaya.stern@choderalab.org>
2019-08-29 15:53:42 -0400

    fixed conda env

--- manuscript-figures/figure-2/run-lsf.sh
+++ manuscript-figures/figure-2/run-lsf.sh
@@ -8,20 +8,20 @@
 #
 # nodes: number of nodes and GPU request
 #BSUB -n 1
-#BSUB -R "rusage[mem=2] span[hosts=1]"
+#BSUB -R "rusage[mem=4] span[hosts=1]"
 #
 # job name (default = name of script file)
-#BSUB -J "drug-bank-wbo[1-10]"
-#BSUB -o /home/chayas/job_output/drugbank-wbo[1-10].stdout
-#BSUB -eo /home/chayas/job_output/drugbank-wbo[1-10].stderr
+#BSUB -J "drug-bank-wbo[1-21]"
+#BSUB -o /home/chayas/job_output/drugbank-wbo.stdout
+#BSUB -eo /home/chayas/job_output/drugbank-wbo.stderr
 
 # Make sure to run bashrc
 source $HOME/.bashrc
 
-export NJOBS=10
+export NJOBS=20
 
 # Activate conda env
-conda activate qcf
+conda activate qcf-manager-openff
 
 # Process dataset fragment
 python generate_wbo.py $LSB_JOBINDEX $NJOBS drugbank-wbo-${LSB_JOBINDEX}.oeb
