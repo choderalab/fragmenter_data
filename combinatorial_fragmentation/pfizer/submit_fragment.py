74dcec305de38709e81f4bde0525a22113befb48
ChayaSt <chaya.stern@choderalab.org>
2019-12-30 11:48:14 -0500

    fragment validation set

---
+++ combinatorial_fragmentation/pfizer/submit_fragment.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import glob
+import itertools
+
+jobs_to_run = glob.glob('../enumerate_states/validation_set/*_states.json')
+
+for dir in jobs_to_run:
+    name = dir.split('/')[-2]
+    print(name)
+
+    with open('fragment.lsf', 'r') as f:
+        filedata = f.read()
+    filedata = filedata.replace('JOB_NAME', name)
+
+    bsub_file = os.path.join(os.getcwd(), '{}_fragment.lsf'.format(name))
+    with open(bsub_file, 'w') as f:
+        f.write(filedata)
+
+    stdin_file = open(bsub_file, 'r')
+    subprocess.Popen(['bsub'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin_file)
+    stdin_file.close()
